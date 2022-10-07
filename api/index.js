6ffcfe3ab0f9b298cb666efc7b525935053ca50f
Carlos Garcia <publicidade.exe@gmail.com>
2022-10-07 15:25:10 -0300

    Dest7

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const path = require("path");
 const app = express();
+const resources = require('../services/resources');
 
 app.use('/static', express.static('public'))
 
 app.get('/api', (req, res) => {
+  const now = new Date().toUTCString();
+  const path = `/api/item/${now}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/resources', async function(req, res, next) {
+    try {
+        res.json(await resources.getMultiple(1));
+    } catch (err) {
+        console.error(`Error while getting resources `, err.message);
+        next(err);
+    }
+});
+
+app.get('/api/resources', (req, res) => {
   const now = new Date().toUTCString();
   const path = `/api/item/${now}`;
   res.setHeader('Content-Type', 'text/html');
