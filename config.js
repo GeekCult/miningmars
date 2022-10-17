bf4272e3ca3a42cb86eb8328827feee4df347eb7
Carlos Garcia <publicidade.exe@gmail.com>
2022-10-17 14:21:58 -0300

    Config

--- config.js
+++ config.js
@@ -1,25 +1,12 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
+    host: "127.0.0.1",
     user: "mars",
     password: "S0mKl23gNec43",
     database: "mars"
-  },
-  db2: {
-    /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "mars"
-  },
-  db3: {
-    /* don't expose password or any sensitive info, done only for demo */
-    host: "162.241.78.8",
-    user: "geekcultcom_mars",
-    password: "M4Rsj98$Acg#dY%r4#H@t09f3*r@",
-    database: "geekcultcom_mars"
-  },
+  }
+  
   listPerPage: 10
 };
 module.exports = config;
